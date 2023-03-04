f857c3e7fc1db6ad55afd547b3c95be95f4a3ea5
Gyanateet Dutta <gyanateet@gmail.com>
2023-03-04 12:57:59 +0530

    added tf  test script
    This script is for testing devices while working with the TensorFlow Framework.

---
+++ cuda_scripts_for_cloud_systems_tf.py
@@ -0,0 +1,60 @@
+import torch
+import timeit
+import tensorflow as tf
+
+print("Torch CUDA: ", torch.cuda.is_available())
+
+
+print("Torch current GPU: ",torch.cuda.current_device())
+
+
+print("Torch GPU: ",torch.cuda.device(0))
+
+
+print("Torch GPU count: ",torch.cuda.device_count())
+
+
+print("Torch GPU name: ",torch.cuda.get_device_name(0))
+
+
+
+if tf.test.is_built_with_cuda():
+    import tensorflow as tf
+    print("Tensorflow GPU Name: ", tf.test.gpu_device_name())
+    print("Tensorflow GPU Device: ", tf.config.list_physical_devices('GPU') )
+
+
+device_name = tf.test.gpu_device_name()
+if device_name != '/device:GPU:0':
+  print(
+      '\n\nThis error most likely means that this notebook is not '
+      'configured to use a GPU.  Change this in Notebook Settings via the '
+      'command palette (cmd/ctrl-shift-P) or the Edit menu.\n\n')
+  raise SystemError('GPU device not found')
+
+def cpu():
+  with tf.device('/cpu:0'):
+    random_image_cpu = tf.random.normal((100, 100, 100, 3))
+    net_cpu = tf.keras.layers.Conv2D(32, 7)(random_image_cpu)
+    return tf.math.reduce_sum(net_cpu)
+
+def gpu():
+  with tf.device('/device:GPU:0'):
+    random_image_gpu = tf.random.normal((100, 100, 100, 3))
+    net_gpu = tf.keras.layers.Conv2D(32, 7)(random_image_gpu)
+    return tf.math.reduce_sum(net_gpu)
+  
+# We run each op once to warm up; see: https://stackoverflow.com/a/45067900
+cpu()
+gpu()
+
+# Run the op several times.
+print('Time (s) to convolve 32x7x7x3 filter over random 100x100x100x3 images '
+      '(batch x height x width x channel). Sum of ten runs.')
+print('CPU (s):')
+cpu_time = timeit.timeit('cpu()', number=10, setup="from __main__ import cpu")
+print(cpu_time)
+print('GPU (s):')
+gpu_time = timeit.timeit('gpu()', number=10, setup="from __main__ import gpu")
+print(gpu_time)
+print('GPU speedup over CPU: {}x'.format(int(cpu_time/gpu_time)))
